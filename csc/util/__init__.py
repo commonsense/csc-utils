6382b6c8d12d299695b140bd8b3d24caba5ce4fa
Rob Speer <rspeer@mit.edu>
2010-09-28 13:51:46 -0400

    merging in csc.nl

--- csc/util/__init__.py
+++ csc/util/__init__.py
@@ -1,17 +1,7 @@
-from csc.util.batch import chunk, Status, ForEach, foreach, queryset_foreach
 from csc.util.cache import cached
 from csc.util.seq import sampling_sequence
 
 __all__ = ['chunk', 'Status', 'ForEach', 'foreach', 'queryset_foreach', 'cached', 'sampling_sequence']
-
-def register_admin(module_name, model_class_names):
-    # I think this was actually a bad idea. It will be going away sometime.
-    from django.contrib import admin
-    module = __import__(module_name, {}, {}, [''])
-    for model_class_name in model_class_names:
-        model = getattr(module, model_class_name)
-        admin_class = getattr(module, model_class_name+'Admin', None)
-        admin.site.register(model, admin_class)
 
 def run_doctests(mod):
     """
