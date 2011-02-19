72d4757b43d681d7d416489065fa1f3708d72334
Rob Speer <rspeer@mit.edu>
2011-02-18 19:11:46 -0500

    even more path hackery for django models

--- csc/conceptnet4/__init__.py
+++ csc/conceptnet4/__init__.py
@@ -1,5 +1,6 @@
 import os
 import conceptnet
-__path__ = [os.path.dirname(conceptnet.__file__)]
+__path__ = [os.path.dirname(__file__), os.path.dirname(conceptnet.__file__)]
+mypath = __path__
 globals().update(conceptnet.__dict__)
-
+__path__ = mypath
