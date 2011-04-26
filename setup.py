deeb7e529d7357bde805d48f5f26bb78dbaa352c
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2011-04-26 13:35:02 -0400

    0.6.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Includes:
 
 Plus a few more odds and ends."""
 
-version_str = '0.6'
+version_str = '0.6.1'
 languages = ['pt', 'nl', 'ja', 'en', 'fi', 'ko', 'fr', 'ar', 'it', 'es', 'hu', 'zh', 'mblem']
 packages=['csc_utils', 'csc', 'csc.conceptnet', 'csc.conceptnet4',
           'csc.concepttools', 'csc.corpus', 'csc.divisi2', 'csc.divisi2.algorithms',
